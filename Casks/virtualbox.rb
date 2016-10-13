a58d2debd2664927fad28e5563754dc81f52d762
Shai Rosenfeld <shaiguitar@gmail.com>
2016-10-13 15:15:53 -0400

    hardcode 4.3 for vagrant

--- Casks/virtualbox.rb
+++ Casks/virtualbox.rb
@@ -1,14 +1,14 @@
 cask 'virtualbox' do
-  if MacOS.version <= :lion
+#  if MacOS.version <= :lion
     version '4.3.38-106717'
     sha256 'f7229f9537dc2359e2f86b2d3623e5024f11d2ce9b075fe34a71b55995897463'
-  elsif MacOS.version == :mountain_lion
-    version '5.0.26-108824'
-    sha256 'e8836a98adea9350917a41e754dfec4fe2df7c4a0224fd8beca72cbc5d778437'
-  else
-    version '5.1.6-110634'
-    sha256 '3fc6e2f1eb6d25ccf91194a102d2dd22b51a23df469257ea4d3893e1c5056e85'
-  end
+#  elsif MacOS.version == :mountain_lion
+#    version '5.0.26-108824'
+#    sha256 'e8836a98adea9350917a41e754dfec4fe2df7c4a0224fd8beca72cbc5d778437'
+#  else
+#    version '5.1.6-110634'
+#    sha256 '3fc6e2f1eb6d25ccf91194a102d2dd22b51a23df469257ea4d3893e1c5056e85'
+#  end
 
   url "http://download.virtualbox.org/virtualbox/#{version.sub(%r{-.*}, '')}/VirtualBox-#{version}-OSX.dmg"
   appcast 'http://download.virtualbox.org/virtualbox/LATEST.TXT',
